8db5a93a59909a42c4ecf174bcfa31b1eef3cc95
Takayoshi Tanaka <tanaka733@outlook.com>
2019-04-04 17:30:00 +0900

    単価モデルクラス追加。

---
+++ ConsoleApp/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Person
+    {
+        internal int PricePerPerson { get; } = 2400;
+    }
+}

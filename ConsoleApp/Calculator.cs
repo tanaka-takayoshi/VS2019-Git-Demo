3f0a5f448c9295f64c96ba8584084f9b68c4a8f7
Takayoshi Tanaka <tanaka733@outlook.com>
2019-04-04 17:24:47 +0900

    ロジック追加。

---
+++ ConsoleApp/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Calculator
+    {
+        public int PricePerPerson { get; } = 2400;
+
+        public int CalculatePrice(int numOfPersons)
+        {
+            return PricePerPerson * numOfPersons;
+        }
+    }
+        
+}
